92c1abb4458f8c67e1595e03cab7b0c591ee1da9
ItsShinnyDreamlz <92038995+ItsShinnyDreamlz@users.noreply.github.com>
2021-10-06 16:52:26 +0200

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,13 +4,13 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || "-", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/SchEbTFaNM", //Donot change..
-  Token: process.env.Token || "Your_Token_Here", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "Your_Client_Id_Here", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "Your_Client_Secret_Here", //Discord Client Secret
+  Token: process.env.Token || "ODk1MzEwNzA2NzI2MDIzMjQ4.YV2tEg.S1otOGQG0al7_j6IPBFQNoBMhYw", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "895310706726023248", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "QNSfxKdh5YJIQr3I7JRipRXwz2zd5HdR", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "ChangeIt", //Safety for your bot its like a password
+  CookieSecret: "Caca1987", //Safety for your bot its like a password
   IconURL:
     "https://media.discordapp.net/attachments/892329976114864149/892424875082407976/image0.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
@@ -29,7 +29,7 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "c60652f174c4462ab973acfbf650010a", //Spotify Client ID
+    ClientID: process.env.Spotify_ClientID || "58b6da44042a41798585ee26d47566eb", //Spotify Client ID
     ClientSecret: process.env.Spotify_ClientSecret || "5a417565c72545cf83711bbb84dd39f4", //Spotify Client Secret
     // I have Provided a Spotify Server too. 😁
   },
