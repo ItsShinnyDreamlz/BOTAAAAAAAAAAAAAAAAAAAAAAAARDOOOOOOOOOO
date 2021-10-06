d30e1a36dee28cafc517e2d52cc8a2bb26f7ed09
ItsShinnyDreamlz <92038995+ItsShinnyDreamlz@users.noreply.github.com>
2021-10-06 17:07:05 +0200

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   Admins: ["668818456673779737"], //Admins of the bot
   ExpressServer: true,//If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || "-", //Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || "/", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/SchEbTFaNM", //Donot change..
   Token: process.env.Token || "ODk1MzEwNzA2NzI2MDIzMjQ4.YV2tEg.S1otOGQG0al7_j6IPBFQNoBMhYw", //Discord Bot Token
@@ -9,7 +9,7 @@ module.exports = {
   ClientSecret: process.env.Discord_ClientSecret || "QNSfxKdh5YJIQr3I7JRipRXwz2zd5HdR", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
-  "24/7": false, //If you want the bot to be stay in the vc 24/7
+  "24/7": true, //If you want the bot to be stay in the vc 24/7
   CookieSecret: "Caca1987", //Safety for your bot its like a password
   IconURL:
     "https://media.discordapp.net/attachments/892329976114864149/892424875082407976/image0.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
